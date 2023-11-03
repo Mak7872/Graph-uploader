2be12c6453b19ecf9bcf980ed2d51b959e41c381
Mak7872 <134184848+Mak7872@users.noreply.github.com>
2023-11-03 17:32:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -11,20 +11,39 @@ class Database:
 
     async def add_user(self, user_id):
         user = self.new_user(user_id)
-        await self.col.insert_one(user)
+        try:
+            async with self._client.start_session() as session:
+                async with session.start_transaction():
+                    await self.col.insert_one(user, session=session)
+        except Exception as e:
+            print(f"Error adding user: {e}")
 
     async def is_user_exist(self, user_id):
-        user = await self.col.find_one({'id': int(user_id)})
-        return True if user else False
+        try:
+            user = await self.col.find_one({'id': int(user_id)})
+            return user is not None
+        except Exception as e:
+            print(f"Error checking user existence: {e}")
+            return False
 
     async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
+        try:
+            count = await self.col.count_documents({})
+            return count
+        except Exception as e:
+            print(f"Error getting total users count: {e}")
+            return 0
 
     async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+        try:
+            all_users = self.col.find({})
+            return all_users
+        except Exception as e:
+            print(f"Error getting all users: {e}")
+            return []
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
-
+        try:
+            await self.col.delete_many({'id': int(user_id)})
+        except Exception as e:
+            print(f"Error deleting user: {e}")
